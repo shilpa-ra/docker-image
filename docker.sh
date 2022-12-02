de75ac7253bb57395aab9d0282ea9e4e9e8595d4
shilpa-ra <113978733+shilpa-ra@users.noreply.github.com>
2022-12-02 22:51:45 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,7 +2,7 @@
 sudo apt update
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-sudo usermod -aG docker devops'
-'sudo systemctl enable docker.service'
-'sudo systemctl start docker'
-'sudo docker info'
+sudo usermod -aG docker devops
+sudo systemctl enable docker.service
+sudo systemctl start docker
+sudo docker info
